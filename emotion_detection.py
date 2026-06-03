2d085d3d5c9f9a2e4c25c4664f256af73c8afcd4
arnabmukherjee944-a11y <arnabmukherjee944@gmail.com>
2026-06-03 14:48:58 -0400

    Formatted emotion detector output

--- emotion_detection.py
+++ emotion_detection.py
@@ -20,4 +20,17 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=input_json, headers=headers)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
